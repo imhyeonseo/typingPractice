fc03c6f6da13b6b9eac9f739c0e5ce07f9de85f5
Void <lollohol755@gmail.com>
2025-08-22 11:12:19 +0900

    first commit

---
+++ js/typing.js
@@ -0,0 +1,7 @@
+const wordbutton = document.getElementById('button1');
+
+const wordtyping = function(){
+    window.location.href = 'word.html';
+}
+
+wordbutton.addEventListener('click', wordtyping)
